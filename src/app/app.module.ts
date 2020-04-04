c582a93e249b8f57462cf800944c1763f6722294
Miguel Torres <miguetv17@gmail.com>
2020-04-04 10:32:45 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PeopleComponent } from './people/people.component';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PeopleComponent, PersonComponent
   ],
   imports: [
     BrowserModule
