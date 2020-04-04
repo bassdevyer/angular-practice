a4b23dd34569c64eeefcdfc86cce7af5f1330bff
Miguel Torres <miguetv17@gmail.com>
2020-04-04 11:24:47 -0500

    Applying styles

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-people',
-  templateUrl: './people.component.html'
+  templateUrl: './people.component.html',
+  //styleUrls: ['./people.component.css']
+  styles: [`
+  h1 {
+    color: blue
+  }
+  `]
 })
 export class PeopleComponent {
 
